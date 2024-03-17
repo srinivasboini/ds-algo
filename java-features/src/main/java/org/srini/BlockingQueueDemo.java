cb480d45e09f31a37c5cfb8254224b086b897a3c
srinivas boini <srinivasboini7@gmail.com>
2024-03-17 17:31:47 +0800

    blocking queue demo

---
+++ java-features/src/main/java/org/srini/BlockingQueueDemo.java
@@ -0,0 +1,39 @@
+package org.srini;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.IntStream;
+
+public class BlockingQueueDemo {
+
+    private static final ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(10) ;
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            IntStream
+                    .range(0,5)
+                    .forEach(e -> {
+                        try {
+                            System.out.println("Producing "+e);
+                            arrayBlockingQueue.put(e);
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    });
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true){
+                    Integer e = arrayBlockingQueue.take();
+                    System.out.println("Consumed "+e);
+                }
+
+
+
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }).start();
+    }
+}
